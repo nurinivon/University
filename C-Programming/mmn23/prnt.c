27ef3a2a37f908582c4af6ceb3e9faef56c2031c
‪Nuri Nivon‬‏ <83066914+nurinivon@users.noreply.github.com>
2021-05-25 13:31:26 +0300

    Update prnt.c

--- C-Programming/mmn23/prnt.c
+++ C-Programming/mmn23/prnt.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+Nuri Nivon
+this program is printing to the output the file name of the source code file (.c) which compiled to create the executable image.
+this program will work under 2 assumptions:
+1. the executable image has the same name as the source code file only just with no extention
+2. the source code file is in the same folder with the executable image
+*/
 int main(int argc, char** args)
 {
 	char* fileName = args[0];
